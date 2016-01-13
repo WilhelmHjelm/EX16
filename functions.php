ac26c7eb8488cd6c9562bc4a60b693f5ea860e57
Knewtn <email@knewt.se>
2016-01-13 14:36:20 +0100

    Remove underscores responsive navigation

--- functions.php
+++ functions.php
@@ -144,8 +144,6 @@ function ex16_scripts() {
 		wp_enqueue_script('jquery');
 	}
 
-	wp_enqueue_script( 'ex16-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'ex16-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
