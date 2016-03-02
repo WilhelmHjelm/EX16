df6bb6f68549c7f0224953a2c4dc643cefb70508
Andreas Antonsson <hello@andreasantonsson.se>
2016-03-02 15:43:50 +0100

    Smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 // Hamburger icon toggle
-// #####################
 $(document).ready(function(){
  $('#hamburger').click(function(){
    $(this).toggleClass('open');
  $('#fixed-nav').toggleClass('open');
 
  });
+});
+
+// Smooth scroll
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
 });
