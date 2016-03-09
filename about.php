04f144a9bbf5d252c09cacf7d820b89db9c41f68
Andreas Antonsson <hello@andreasantonsson.se>
2016-03-09 10:15:06 +0100

    Output project groups member by categories

--- about.php
+++ about.php
@@ -24,29 +24,48 @@ get_header(); ?>
 </div> <!-- .container -->
 
 <div class="container">
-  <div class="twelve columns">
-    <h2>Projektgrupperna</h2>
-  </div> <!-- .twelve -->
+  <div class="ten columns offset-by-one">
 
-  <?php
-  // Custom post type "Examensklassen" list
-
+    <?php
         $args = array(
-          'post_type' => 'examensklassen'
-        );
-        $examensklassen = new WP_Query( $args );
-        if( $examensklassen->have_posts() ) {
-          echo '<ul id="og-grid" class="og-grid twelve columns">';
-          $i = 1;
-          while( $examensklassen->have_posts() ) {
-            $examensklassen->the_post();
-            if($i == 1) {$graduateColor = "blue";}
+            'type'      => 'projektgruppen',
+            'taxonomy'  => 'category',
+            'orderby'   => 'ID',
+            'order'     => 'DESC',
+         );
+
+      $categories = get_categories( $args );
+
+      foreach($categories as $cat){ ?>
+          <div class="twelve columns"><h3><?php echo $cat->name; ?></h3></div>
+
+<?php
+      $catslug = $cat->slug;
+      $category_query_args = array(
+        'post_type' => 'projektgruppen',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'category',
+                'field' => 'slug',
+                'terms' => $catslug,
+            ),
+        ),
+      );
+      $category_query = new WP_Query( $category_query_args );
+
+      if ( $category_query->have_posts() ) {
+        echo '<ul class="og-grid project-groups twelve columns">';
+        $i = 1;
+        while ($category_query->have_posts()) {
+          $category_query->the_post();
+
+          if($i == 1) {$graduateColor = "blue";}
             if($i == 2) {$graduateColor = "turquoise";}
             if($i == 3) {$graduateColor = "green";}
             if($i == 4) {$graduateColor = "orange"; $i = 0;}
             $i++;
 
-            $attachment_id = get_field('bild');
+          $attachment_id = get_field('bild');
             $thumb = "medium"; // (thumbnail, medium, large, full or custom size)
             $thumb_image = wp_get_attachment_image_src( $attachment_id, $thumb );
             $fullsize = "full"; // (thumbnail, medium, large, full or custom size)
@@ -55,22 +74,22 @@ get_header(); ?>
             $hover_attachment_id = get_field('hoverbild');
             $hover_thumb = "medium";
             $hover_thumb_image = wp_get_attachment_image_src( $hover_attachment_id, $hover_thumb );
-            ?>
-            <li>
-              <a href="" data-largesrc="<?php echo $full_image[0]; ?>" data-title="<?php the_title() ?>" data-description='<?php the_content() ?>'>
-                <img src="<?php echo $thumb_image[0]; ?>" class="static" alt="<?php the_title() ?>">
-                <img src="<?php echo $hover_thumb_image[0]; ?>" class="hover" alt="<?php the_title() ?>">
-                <div class="graduate-name <?php echo $graduateColor; ?>-bg"><span><?php the_title() ?></span></div>
-              </a>
-            </li>
-          <?php
-          }
-          echo '</ul>';
+          ?>
+
+          <li>
+            <a href="">
+              <img src="<?php echo $thumb_image[0]; ?>" class="static" alt="<?php the_title() ?>">
+              <img src="<?php echo $hover_thumb_image[0]; ?>" class="hover" alt="<?php the_title() ?>">
+              <div class="graduate-name <?php echo $graduateColor; ?>-bg"><span><?php the_title() ?></span></div>
+            </a>
+          </li>
+
+        <?php }
+        echo '</ul> <!-- .og-grid .twelve -->';
       }
-      else {
-        echo 'Det finns inga från examensklassen att visa!';
-      }
-    ?>
+
+      ?>
+ <?php } /* End categories */ ?>
 
 </div> <!-- .container -->
 
