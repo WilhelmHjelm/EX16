5d491e8239dfce75c92787575d890f445573941f
Wilhelm Hjelm <wilhelm@hjelms.se>
2016-03-18 21:44:40 +0100

    moved splash out of container

--- graduates.php
+++ graduates.php
@@ -13,9 +13,8 @@ get_header(); ?>
 
 <?php endwhile; // end of the loop. ?>
 
-
+<img class="splash" id="splash-graduates" src="<?php echo get_template_directory_uri(); ?>/img/splash-10.svg" alt="splash">
 <div class="container">
-  <img class="splash" id="splash-graduates" src="<?php echo get_template_directory_uri(); ?>/img/splash-10.svg" alt="splash">
   <div class="twelve columns">
   <?php the_title( '<h1 class="entry-title blue-text">', '</h1>' ); ?>
   </div>
