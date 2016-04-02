9003969c4ee68fa141125c8be690f2f41799db67
Andreas Antonsson <hello@andreasantonsson.se>
2016-04-02 15:26:21 +0200

    Basic archive grid layout

--- archive-utstallning.php
+++ archive-utstallning.php
@@ -9,30 +9,22 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="utstallning" class="container">
+		<div class="utstallning-filter twelve columns">
+			filter
+		</div>
 
 		<?php if ( have_posts() ) : ?>
-detta funka ju
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
+				<div class="grid-item">
+					<a href="<?php echo the_permalink(); ?>">
+						<?php echo the_title(); ?>
+					</a>
+				</div>
 
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_format() );
-				?>
+
 
 			<?php endwhile; ?>
 
@@ -44,8 +36,6 @@ detta funka ju
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div><!-- #exhibition .container -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
