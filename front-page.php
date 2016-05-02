e28a96c7a1975f73cd202dff8e8b286c1a9abc86
Andreas Antonsson <hello@andreasantonsson.se>
2016-05-02 12:57:29 +0200

    schedule + updated ex by night text

--- front-page.php
+++ front-page.php
@@ -31,39 +31,38 @@ get_header(); ?>
     </div> -->
     <div class="row">
     <div class="twelve columns">
-      <h2 class="green-text">Schemat<br> presenteras den 2 maj</h2>
+      <h2 class="green-text">Schemat</h2>
     </div> <!-- .twelve .columns -->
   </div> <!-- .row -->
   <div class="row">
-    <?php /*
+
     <div class="three columns thursday">
         <h4 class="turquoise-text">Torsdag</h4>
         <ul class="tab-content">
-            <li>09:00<br><strong>Invigning av EX16</strong><br><span>Äger rum i Färgeriet, Kåkenhus.</span></li>
-            <li>09:00<br><strong>Utställningen öppnar</strong></li>
-            <li>10:15-12:00<br><strong>Föreläsning i K4</strong><br><span>Bo Bergström talar om saker man kan göra med en katt.</span></li>
-            <li>18:00<br><strong>Företagskvällen börjar</strong><br><span>Läs mer om vad som händer.</span></li>
-            <li>21:00<br><strong>Utställningen och företagskvällen stänger</strong></li>
+            <li>13:15<br><strong>Invigning av EX16</strong><br><span>Färgeriet, Kåkenhus</span></li>
+            <li>13:15-17:00<br><strong>Utställningen är öppen</strong></li>
+            <li>18:00<br><strong>EX BY NIGHT - Företagskvällen öppnar</strong><br><span>Färgeriet, Kåkenhus</span></li>
+            <li>18:00<br><strong>Föreläsning med Mia Lindstam, Gerillabyrån</strong><br><span>K4, Kåkenhus</span></li>
+            <li>19:15<br><strong>Lär dig mingla med Demola</strong><br><span>Färgeriet, Kåkenhus</span></li>
+            <li>19:30-21.00<br><strong>Mingel</strong></li>
         </ul>
     </div> <!-- .columns -->
     <div class="three columns friday">
         <h4 class="green-text">Fredag</h4>
         <ul class="tab-content">
-            <li>09:00<br><strong>Utställningen öppnar</strong></li>
-            <li>12:15-13:00<br><strong>Föreläsning i K4</strong><br><span>Crazy Pictures talar om för dig vad du vill veta.</span></li>
-            <li>13:15-15:00<br><strong>Föreläsning i K4</strong><br><span>Lars Winnerbäck kommer och gästspelar.</span></li>
-            <li>18:00<br><strong>Utställningen stänger</strong></li>
+            <li>09:00<br><strong>Frukostföreläsning med DIK</strong><br><span>Färgeriet, Kåkenhus</span></li>
+            <li>10:00-17:00<br><strong>Utställningen är öppen</strong></li>
+            <li>10:40<br><strong>Föreläsning med Futurniture</strong><br><span>K4, Kåkenhus</span></li>
+            <li>13:15<br><strong>Föreläsning med Crazy Pictures</strong><br><span>K4, Kåkenhus</span></li>
         </ul>
     </div> <!-- .columns -->
     <div class="three columns saturday">
         <h4 class="orange-text">Lördag</h4>
         <ul class="tab-content" id="content3">
-          <li>10:00<br><strong>Utställningen öppnar</strong></li>
-          <li>14:00<br><strong>Utställningen stänger</strong></li>
-          <li>19:00<br><strong>Examenssittning <a href="https://www.facebook.com/events/592502144240766/" target="_blank"><i class="fa fa-facebook"></i></a></strong><br><span>Hålls på Laxholmen och är examensklassens avslutande sittning.</span></li>
+          <li>10:00-15.00<br><strong>Utställningen är öppen</strong></li>
         </ul>
     </div> <!-- .columns -->
-    */ ?>
+
     </div> <!-- .row -->
   </div> <!-- .container -->
 
@@ -125,10 +124,14 @@ get_header(); ?>
         <img src="<?php echo get_template_directory_uri(); ?>/img/foretagskvall.jpg" alt="EX16 Företagskväll">
       </div> <!-- .columns -->
       <div class="five columns">
-        <p>Den 12 maj annordnar EX16 en företagskväll då bandet mellan universitet och näringsliv ska växa sig starkare.
-          Här formas möjligheter till att knyta värdefulla kontakter inför framtiden och det är även examenklassens
-          tillfälle att visa upp sina allra starkaste projekt. Kvällen kommer ta plats i Kåkenhus på Campus Norrköping
-          och inledas med en förläsning följt av mingel, tilltugg och uppvisning av alster.<br><br>
+        <p>Den 12 maj annordnas företagskvällen EX by night som har som syfte att skapa ett starkare band mellan universitetet
+          och näringslivet. Under kvällen kommer examensklassen att visa upp sina starkaste projekt i en fantastisk utställning
+          samt få en chans att mingla med olika inbjudna företag från branschen.<br><br>
+          Kvällen kommer inledas med en föreläsning med Mia Lindstam från Gerillabyrån som ger tips och beskriver faktorerna
+          bakom en lyckad gerillakampanj. Sedan kommer Demola East Sweden att dela med sig av sina bästa mingeltips innan
+          minglet mellan företag och studenter drar igång på riktigt. Här formas möjligheter till att knyta värdefulla
+          kontakter inför framtiden!<br><br>
+
           Är du eller ditt företag intresserade av att träffa branschens framtida talanger? Maila
           <a href="mailto:foretagskvall@ex16.se">foretagskvall@ex16.se</a> för mer information.
         </p>
